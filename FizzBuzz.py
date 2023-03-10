bdd0e0c0719b415c0c00ce44fae08e3d88d97b6e
Ireland Lyerla <lyerlajd@gmail.com>
2023-03-10 14:12:29 -0600

    added comments and renamed check function to fizzbuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,18 +1,28 @@
 def main():
+    # initializing counter
     counter = 1
 
+    # looping through counter until it equals 100
     while(counter <= 100):
-        print(check(counter))
+        # calling function to check if divisible by 3 or 5, then printing response
+        print(fizzbuzz(counter))
+        # incrementing counter
         counter += 1
 
-def check(number):
+def fizzbuzz(number):
+    # if divisible by 3 and 5, returning FizzBuzz
     if (number % 3 == 0 and number % 5 == 0):
         return "FizzBuzz"
+    # if only divisible by 3, returning Fizz
     elif (number % 3 == 0):
         return "Fizz"
+    # if only divisible by 5, returning Buzz
     elif (number % 5 == 0):
         return"Buzz"
+    # number was not divisble by 3 or 5, just returning number
     else:
         return number
 
+
+# calling the main function
 main()
